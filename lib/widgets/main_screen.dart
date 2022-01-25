3b8ac92046eebf9df04718e0c252f225a07698cd
Piyush <piyushsvps@gmail.com>
2022-01-25 11:55:33 +0530

    Added a temporary text on Main Screen so that you know its Main Screen :P

--- lib/widgets/main_screen.dart
+++ lib/widgets/main_screen.dart
@@ -6,6 +6,20 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
+      height: double.infinity,
+
+      child: Center(
+        child: Text(
+          "Main Screen",
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: "segoe",
+            fontSize: 32,
+            fontWeight: FontWeight.w600
+          ),
+        )
+      ),
 
       decoration: BoxDecoration(
         color: Color(0xFF36393F),
